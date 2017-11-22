24637652fb6197b6b8b4fc584b5be9c2b1a9d3ea
Edimarcordero0220 <edicordero98@gmail.com>
2017-11-23 00:01:09 +0300

    Parcial2

---
+++ Entidades/PresupuestoDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class PresupuestoDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PresupuestoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Meta { get; set; }
+        public int Logrado { get; set; }
+    }
+}
---
+++ Entidades/Presupuestos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Presupuestos
+    {
+        [Key]
+        public int PresupuestoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
